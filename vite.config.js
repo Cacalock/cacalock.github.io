1f43d10df847853dfd3b046a25a4633b8efc0ff8
Cacalock <cassianebeatrizdarosa@hotmail.com>
2026-07-03 21:25:23 -0300

    ajustes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/Tecnologias-Para-Desenvolvimento-Web-Somativa-2/',
-})
+    plugins: [react()],
+    base: '/',
+});
